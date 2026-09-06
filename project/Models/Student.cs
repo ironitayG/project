488de64dd509f692eb1cd4f2f2e3847d17bc5f2a
ironitayG <itayarde@gmail.com>
2026-09-06 16:28:14 +0300

    Add project files.

---
+++ project/Models/Student.cs
@@ -0,0 +1,10 @@
+namespace project.Models
+{
+    public class Student
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public double MathScore { get; set; }
+        public double EnglishScore { get; set; }
+    }
+}
